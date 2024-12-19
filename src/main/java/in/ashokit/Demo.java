0afdd6b10b37687f400f4ab87acf8e807f3fb596
Ramya2134 <kolantiramya@gmail.com>
2024-12-19 14:58:14 +0530

    his-300:Releted changes

--- src/main/java/in/ashokit/Demo.java
+++ src/main/java/in/ashokit/Demo.java
@@ -3,6 +3,11 @@ package in.ashokit;
 public class Demo extends RuntimeException {
 	int a =10;
     int b =20;
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hi");
+    }
     
 
 }
